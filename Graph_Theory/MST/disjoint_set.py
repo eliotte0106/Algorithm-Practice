4a5ca8bd39ca226371b53485c96dacd0ddf15404
Eliotte <72038638+eliotte0106@users.noreply.github.com>
2021-03-24 10:36:17 -0400

    Add files via upload

--- Graph_Theory/MST/disjoint_set.py
+++ Graph_Theory/MST/disjoint_set.py
@@ -1,1 +1,40 @@
+#find the set that includes a particular element
+def find_parent(parent, x):
+    #if not a root node, recursive call until find the root node
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x] #path compression
+
+#union the set that includes two elements
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b :
+        parent[b] = a
+    else:
+        parent[a] = b
+
+#input of num of nodes and edges(union)
+v,e = map(int,input().split())
+parent = [0] * (v+1) #parent table
+
+#parent table
+for i in range(1,v+1):
+    parent[i] = i
+
+#union process
+for i in range(e):
+    a,b = map(int,input().split())
+    union_parent(parent,a,b)
+
+#print the set that each element in included
+print("set of each element: ", end='')
+for i in range(1,v+1):
+    print(find_parent(parent,i),end=' ')
+print()
+
+#print parent table
+print("parent table: ",end='')
+for i in range(1, v+1):
+    print(parent[i],end=' ')
 
