1c6cf5fcd53a1e9760d6293809d6bba3f2c42b4f
Eliotte <72038638+eliotte0106@users.noreply.github.com>
2021-03-25 11:33:32 -0400

    Add files via upload

---
+++ Graph_Theory/MST/city_partition.py
@@ -0,0 +1,39 @@
+"""
+divide one city into two cities that have cheap info.
+"""
+def find_parent(parent,x):
+    if parent[x] != x :
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v,e = map(int,input().split())
+
+edges = []
+result = 0
+
+for i in range(1,v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a,b,cost = map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+last = 0 # expensive edge
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent,a) != find_parent(parent,b):
+        union_parent(parent,a,b)
+        result += cost
+        last = cost
+
+print(result - last)
