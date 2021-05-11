a341610365157218bb98793a1233ba2ae03d1ea1
Eliotte Park <jihong2253@daum.net>
2021-05-11 03:10:16 -0400

    Add files via upload

---
+++ Sorting/insertion.py
@@ -0,0 +1,10 @@
+#insertion sort
+def insertion(arr):
+    for i in range(1,len(arr)):
+        for j in range(i,0,-1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+    return arr
+print(insertion([7,5,9,0,3,1,6,2,4,8])) 
