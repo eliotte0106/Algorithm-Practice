331ec7a7091da00b7a46b4e880c05029959515d3
Eliotte <jihong2253@daum.net>
2021-05-21 18:19:47 -0400

    Add files via upload

---
+++ Search/27_2.py
@@ -0,0 +1,25 @@
+'''
+count how many x is in the list. The runtime should be O(logN).
+ex)
+[1,1,2,2,2,2,3]
+should output 4
+solution: last index of 2 - first index of 2 will be the answer using binary search
+'''
+
+from bisect import bisect_left, bisect_right
+
+def count_by_range(array,left_value,right_value):
+    right_index = bisect_right(array,right_value)
+    left_index = bisect_left(array,left_value)
+    return right_index - left_index
+
+n,x = map(int,input().split())
+array = list(map(int,input().split()))
+
+count = count_by_range(array,x,x)
+
+if count == 0:
+    print(-1)
+
+else:
+    print(count)
