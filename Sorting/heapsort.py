8218101910497e227156542534b60ca2365178d5
Eliotte <jihong2253@daum.net>
2021-07-11 21:38:28 -0400

    Add files via upload

---
+++ Sorting/heapsort.py
@@ -0,0 +1,24 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+def heapsort(iterable):
+    h = []
+    result = []
+
+    for value in iterable:
+        heapq.heappush(h,value)
+    
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+    
+    return result
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+res = heapsort(arr)
+print(res)
