8062c1a52d6acdad4266c3e92f066b91f568964a
Eliotte <jihong2253@daum.net>
2021-12-21 18:24:28 -0500

    Add files via upload

--- BFS&DFS/BFS.py
+++ BFS&DFS/BFS.py
@@ -11,8 +11,14 @@ def bfs(graph,start,visited):
     while queue:
         #print
         v = queue.popleft()
-        print(v)
-        for i in graph(v):
+        print(v, end = " ")
+        for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
+
+visited = [False] * 9
+graph = [[], [2,3,8], [1,7,8], [1,4,5], [3,5], [3,4], [7], [6,8], [1,7]]
+bfs(graph, 1, visited)
+
+#1 2 3 8 7 4 5 6
