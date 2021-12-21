269ae1b8ac03c0e804cf0cbd5515451a2f730c5e
Eliotte <jihong2253@daum.net>
2021-12-21 17:58:54 -0500

    Add files via upload

--- BFS&DFS/DFS.py
+++ BFS&DFS/DFS.py
@@ -1,11 +1,17 @@
 #DFS: use Stack (LIFO), O(N), N = # of vertex
 
-def dfs(graph,v,visited):
+def dfs(graph, v, visited):
     # current node visited
     visited[v] = True
     print(v,end = ' ')
     # visit adjacent vertex recursively
     for i in graph[v]:
         if not visited[i]:
-            dfs(graph,i,visited)
+            dfs(graph, i, visited)
 
+#example
+visited = [False] * 9
+graph = [[], [2,3,8], [1,7,8], [1,4,5], [3,5], [3,4], [7], [6,8], [1,7]]
+dfs(graph, 1, visited)
+
+#1 2 7 6 8 3 4 5
