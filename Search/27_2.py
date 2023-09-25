cc4bbb26748b4c7248feefc25b6be3436a1dfbc7
Jihong E Park <jihong2253@gmail.com>
2023-09-25 12:28:17 -0400

    Update 27_2.py

--- Search/27_2.py
+++ Search/27_2.py
@@ -9,8 +9,8 @@ solution: last index of 2 - first index of 2 will be the answer using binary sea
 from bisect import bisect_left, bisect_right
 
 def count_by_range(array,left_value,right_value):
-    right_index = bisect_right(array,right_value)
-    left_index = bisect_left(array,left_value)
+    right_index = bisect_right(array,right_value) #idx 5 -> this idx is last idx of 2 + 1
+    left_index = bisect_left(array,left_value) #idx 2
     return right_index - left_index
 
 n,x = map(int,input().split())
